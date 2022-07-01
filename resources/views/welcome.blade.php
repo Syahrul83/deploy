96e1bab9043aab7f479ccff9e52acfb7eb0e22c7
syahrul anwar <syahrulanwar83@gmail.com>
2022-07-01 15:55:13 +0800

    rusa ya

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Rusa Gedek Sekali yaa
                 </div>
 
                 <div class="links">
